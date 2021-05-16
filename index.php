90488ebb1f0e28b9f2d36875bb6eb1aeb6bbd438
Breno Ramalho Lemes <breno@br-lemes.net>
2021-05-16 16:20:13 -0400

    Adriana

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Mundo Pet</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css" />
+	</head>
+	<body>
+		<div id="pag">
+			<div id="logo"/></div>
+			<div id="banner" style="text-align:center;"></div>
+			<div id="título">
+				<br/>
+				<h1> O Pet shop mais perto de você<h1/>
+			</div>
+			<div id="login">
+				<iframe scrolling="no" width="250" height="150" frameborder="0" src=""></iframe>
+			</div>
+			<div id="menu">
+				MENU AQUI
+			</div>
+			<div id="conteudo">
+				CONTEUDO AQUI
+			</div>
+			<div id="rodape">
+				RODAPE AQUI
+			</div>
+		</div>
+	</body>
 </html>
