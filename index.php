7659a5b58d34011380138f5b58da1f46ea3404f3
Breno Ramalho Lemes <breno@br-lemes.net>
2021-06-02 15:33:54 -0400

    Products and not found pages

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
 			</div>
 			</div>
 		</nav>
-		<div class="w3-card w3-container w3-content w3-section w3-pale-yellow">
+		<div class="w3-card w3-container w3-content w3-section w3-pale-yellow"><?php
+			$uri = explode("/", $_SERVER["REQUEST_URI"]);
+			if (count($uri) == 2 and $uri[0] == "" and $uri[1] == "") { ?>
 			<div class="w3-row"><?php
 				$db = new PDO("sqlite:mundopet.sqlite3");
 				foreach ($db->query("SELECT * FROM products") as $row) { ?>
@@ -36,7 +38,35 @@
 						<p><?=$row["name"]?></p>
 					</div>
 				</a>
-			<?php } ?></div>
+			<?php } ?></div><?php } elseif (count($uri) == 3 and $uri[0] == "" and $uri[1] == "products") {
+				$db = new PDO("sqlite:mundopet.sqlite3");
+				$stmt = $db->prepare("SELECT * FROM products WHERE id=?");
+				$stmt->execute([$uri[2]]);
+				foreach ($stmt->fetchAll() as $row) { ?>
+				<div class="w3-row">
+					<div class="w3-col m4 w3-panel">
+						<img class="w3-image" src="/images/<?=$row["img"]?>">
+						<p class="w3-center">
+							<a class="w3-button w3-green" href="/buy/<?=$uri[2]?>">Comprar</a>
+						</p>
+					</div>
+					<div class="w3-col m8 w3-panel">
+						<h1><?=$row["name"]?></h1>
+						<h2>R$ <?=$row["price"]?></h2>
+						<?php
+							$description = explode("\n", $row["description"]);
+							foreach ($description as $line) { ?>
+								<p><?=$line?></p><?php
+							}
+						?>
+					</div>
+				</div>
+			<?php } } else { ?>
+				<h1 class="w3-center">Ops!</h1>
+				<h2 class="w3-center">Algo deu errado :(</h2>
+				<p class="w3-center">Não estamos encontrando o que você quer,
+				utilize a nossa busca ou volte à página inicial.</p>
+			<?php } ?>
 		</div>
 		<div class="w3-card w3-container w3-content w3-section w3-pale-yellow">
 			<p class="w3-center">Copyright (c) 2021 Mundo Pet</p>
