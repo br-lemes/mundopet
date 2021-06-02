5ea376b46f2ffb105b089191f069f268d2e58ad2
Breno Ramalho Lemes <breno@br-lemes.net>
2021-06-02 15:31:44 -0400

    Use columns names as index

--- index.php
+++ index.php
@@ -24,17 +24,16 @@
 		<div class="w3-card w3-container w3-content w3-section w3-pale-yellow">
 			<div class="w3-row"><?php
 				$db = new PDO("sqlite:mundopet.sqlite3");
-				foreach ($db->query("SELECT * FROM products") as $row) {
-			?>
-				<a href="/products/<?=$row[0]?>">
-					<div class="w3-col s6 m3 w3-container w3-hover-white">
-						<img class="w3-image" src="/images/<?=$row[4]?>">
+				foreach ($db->query("SELECT * FROM products") as $row) { ?>
+				<a href="/products/<?=$row["id"]?>">
+					<div class="w3-col s6 m3 w3-panel w3-hover-white">
+						<img class="w3-image" src="/images/<?=$row["img"]?>">
 						<p class="w3-right-align">
 							<span class="w3-button w3-light-green w3-hover-light-green">
-								R$ <?=$row[3]?>
+								R$ <?=$row["price"]?>
 							</span>
 						</p>
-						<p><?=$row[1]?></p>
+						<p><?=$row["name"]?></p>
 					</div>
 				</a>
 			<?php } ?></div>
