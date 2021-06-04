590e4e98250b322601fb4b53391dd523db5dc8f3
Breno Ramalho Lemes <breno@br-lemes.net>
2021-06-04 11:24:54 -0400

    Numeric check

--- require/main.php
+++ require/main.php
@@ -2,7 +2,7 @@
 	$uri = explode("/", $_SERVER["REQUEST_URI"]);
 	if (count($uri) == 2 and $uri[0] == "" and $uri[1] == "") {
 		require "require/home.php";
-	} elseif (count($uri) == 3 and $uri[0] == "" and $uri[1] == "products") {
+	} elseif (count($uri) == 3 and $uri[0] == "" and $uri[1] == "products" and is_numeric($uri[2]) ) {
 		require "require/products.php";
 	} else {
 		require "require/404.php";
