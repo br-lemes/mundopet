ae46d73a40167045ae42339c75dcc261bef1ae50
Breno Ramalho Lemes <breno@br-lemes.net>
2021-06-04 13:55:32 -0400

    Simplified

--- require/home.php
+++ require/home.php
@@ -3,17 +3,13 @@
 	$db = new PDO("sqlite:database/mundopet.sqlite3");
 	foreach ($db->query("SELECT * FROM products") as $row) {
 ?>
-	<a href="/products/<?=$row["id"]?>">
-		<div class="w3-col s6 m3 w3-panel w3-hover-white">
-			<img class="w3-image" src="/images/<?=$row["img"]?>">
-			<p class="w3-right-align">
-				<span class="w3-button w3-light-green w3-hover-light-green">
-					R$ <?=$row["price"]?>
-				</span>
-			</p>
-			<p><?=$row["name"]?></p>
-		</div>
-	</a>
+<a class="w3-col s6 m3 w3-panel w3-hover-white" href="/products/<?=$row["id"]?>">
+	<img class="w3-image" src="/images/<?=$row["img"]?>">
+	<p class="w3-right-align">
+		<span class="w3-button w3-light-green">R$ <?=$row["price"]?></span>
+	</p>
+	<p><?=$row["name"]?></p>
+</a>
 <?php
 	}
 ?>
